7605d559fbfb942e57d32b926195fdb788f73ad5
alexSam <alexsamalot19@gmail.com>
2019-10-29 17:53:49 -0400

    minimum viable product

---
+++ bamazon.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products
+(
+  item_id INT NOT NULL
+  AUTO_INCREMENT,
+  product_name VARCHAR
+  (45) NULL,
+  department_name VARCHAR
+  (45) NULL,
+  price FLOAT
+  (45) NULL,
+  stock_quantity FLOAT
+  (10) NULL,
+  PRIMARY KEY
+  (item_id)
+);
+
+
+
+LOAD DATA LOCAL INFILE '/Users/alex/Documents/PREWORK_AMS/CLASS/CLASS102219/Week12-Day03-TopMusic/Top1000Songs.csv' 
+INTO TABLE songs;
+
+SHOW VARIABLES LIKE "secure_file_priv";
+
+  SELECT *
+  FROM songs;
---
+++ bamazonCustomer.js
@@ -0,0 +1,173 @@
+/**
+ * @see {@link https://www.npmjs.com/package/mysql}
+ */
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const keys = require('./keys.js');
+const Table = require('easy-table');
+
+const sqlKey = (keys.SQL.Pass);
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Your password
+  password: sqlKey,
+  database: 'bamazon_db',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+  doCRUD();
+});
+
+/**
+ * Called by callback function upon successful establishment of DB connection.
+ * @see {@link https://www.npmjs.com/package/mysql#performing-queries}
+ */
+
+function doCRUD() {
+    displayTable();
+  getUserInputAsync();
+}
+
+/**
+ * Get the user input using Inquirer lib
+ * @return {Promise} an Inquirer promise that resolves with
+ * an answers object
+ */
+
+
+function getUserInputAsync() {
+
+  return inquirer.prompt([
+    {
+      name: 'type',
+      message: 'Welcome! Would you like to:',
+      type: 'list',
+      choices: ['Order', 'Exit'],
+      default: 'Order',
+    },
+  ]).then(function (inquirerResponse) {
+    console.log(inquirerResponse.type);
+    // getUserInputAsync(inquirerResponse.type);
+    switch (inquirerResponse.type) {
+      case 'Order':
+        placeOrder();
+        break;
+
+      case 'Exit':
+        exitBamazon();
+        break;
+    }
+  }
+
+
+  );
+}
+
+function placeOrder() {
+    
+  return inquirer.prompt([
+    {
+      name: 'order',
+      message: 'What would you like to order? \n',
+    },
+    {
+      name: 'orderAmount',
+      message: 'how much would you like? \n',
+    },
+  ]).then(function (inquirerResponse) {
+    console.log(inquirerResponse.order);
+
+    const query = connection.query(
+      'SELECT * FROM products WHERE ?',
+      [
+        {
+          item_id: inquirerResponse.order,
+        },
+      ],
+      function (err, res) {
+        if (err) throw err;
+
+        let orderID = inquirerResponse.orderAmount;
+        if (inquirerResponse.orderAmount < res[0].stock_quantity) {
+          let newStock = res[0].stock_quantity - inquirerResponse.orderAmount;
+          let orderBill = res[0].price * orderID;
+          console.log("Your order total is: $" + orderBill);
+          updateProduct(newStock, orderID);
+
+        } else {
+          console.log("Insufficient Quantity!")
+          getUserInputAsync()
+        }
+
+        // }
+      }
+
+
+    );
+  }
+  )
+};
+
+function updateProduct(newStock, orderID) {
+
+  const query = connection.query(
+    'UPDATE products SET ? WHERE ?',
+    [
+      {
+        stock_quantity: newStock,
+      },
+      {
+        item_id: orderID,
+      },
+    ],
+    function (err, res) {
+      if (err) throw err;
+      
+
+    }
+  );
+
+  // logs the actual query being run
+//   console.log(query.sql);
+console.log("You're order has been placed! \n")
+  getUserInputAsync();
+}
+
+
+
+function displayTable(){
+    connection.query("Select * FROM products", function(error,results){
+        if (error) throw error;
+
+        var t = new Table;
+        results.forEach(function(product){
+            t.cell('item_id', product.item_id);
+            t.cell('product_name', product.product_name);
+            t.cell('department_name', product.department_name);
+            t.cell('price', product.price);
+            t.cell('stock_quantity', product.stock_quantity);
+            t.newRow();
+        });
+
+        console.log(t.toString());
+
+    })
+   
+};
+
+
+
+function exitBamazon() {
+  console.log("Good bye!");
+  connection.end();
+};
